cccfa0f556c3d233841c3fcee7e9965188944bd8
AndreiCalazans <andreixoc@hotmail.com>
2019-07-28 09:11:46 -0300

    Add text copies

---
+++ src/utilities/texts.tsx
@@ -0,0 +1,15 @@
+export const texts = {
+  app_intro: "This is an example app. It contains 3 screens: Subscription Selection, Account Selection, and User Info.",
+  select_subscriptions: "Select available subscriptions.",
+  select_account: "Select Account",
+  logged_user: "Logged User Information",
+  log_out: "Log Out",
+  subscription_type_solo: "One User Suscription",
+  subscription_type_multiple: "Multiple Users Suscription",
+  logged_in: "Logged In User",
+  subscription_owner: "Subscription Owner",
+  remove_account: "Remove Account",
+  add_account: "Add Account",
+  are_you_sure: "Are you sure?",
+  need_subscription_owner: "You need to be a subscription owner to procceed with this operation"
+}
